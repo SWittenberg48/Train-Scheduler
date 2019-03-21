6d46182882d75733ece2adc3170081f36e40d227
Scott wittenberg <wittenb48@gmail.com>
2019-03-21 14:59:16 -0500

    Got it working!

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -17,21 +17,20 @@ $(document).ready(function() {
   var trainTime = "";
   var frequency = "";
 
- 
   $("#submit-button").on("click", function(event) {
     event.preventDefault();
     trainName = $("#name")
       .val()
       .trim();
-      
+
     destination = $("#destination")
       .val()
       .trim();
-      
+
     trainTime = $("#firstTime")
       .val()
       .trim();
-      
+
     frequency = $("#frequency")
       .val()
       .trim();
@@ -49,27 +48,27 @@ $(document).ready(function() {
     var firstData = snapshot.val().time;
     var frequencyData = snapshot.val().frequency;
 
-  var firstTrainConverted = moment(firstData, "hh:mm").subtract("1, years");
-  var currentTime = moment();
-  var difference = currentTime().diff(moment(firstTrainConverted), "minutes");
-  var remainder = difference % frequencyData;
-  var minUntilTrain = frequencyData - remainder;
-  console.log(remainder);
-  var nextTrain = moment()
-    .add(minUntilTrain, "minutes")
-    .format("hh:mm");
+    var firstTrainConverted = moment(firstData, "hh:mm").subtract("1, years");
+    var currentTime = moment().format("hh:mm");
+    var difference = moment().diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequencyData;
+    var minUntilTrain = frequencyData - remainder;
+    console.log(remainder);
+    var nextTrain = moment()
+      .add(minUntilTrain, "minutes")
+      .format("hh:mm");
 
-// This is where I am getting my error and can not for the life of me find it.Phantom?
-// Trying to append same way I did previously in an assignment for Job data. Says > should not be there.
+    // This is where I am getting my error and can not for the life of me find it.Phantom?
+    // Trying to append same way I did previously in an assignment for Job data. Says > should not be there.
 
-  $("#final-append").append(
-    <tr>
+    $("#final-append").append(
+      `<tr>
         <td>${trainData}</td>
       <td>${destData}</td>
       <td>${frequencyData}</td>
       <td>${moment(nextTrain).format("hh:mm")}</td>
       <td>${minUntilTrain}</td>
-      </tr>
-  );
-
+      </tr>`
+    );
   });
+});
